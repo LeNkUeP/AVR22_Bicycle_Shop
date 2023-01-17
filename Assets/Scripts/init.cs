c217e234cac895bde78ef46b3e621c7a73c17d8f
Lennart Küppers <lennartkueppers@web.de>
2023-01-17 22:52:33 +0100

    init script now pays attention to editor or build

--- Assets/Scripts/init.cs
+++ Assets/Scripts/init.cs
@@ -5,11 +5,14 @@ public class init : MonoBehaviour
 {
     void Start()
     {
-        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        //SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Workshop Area", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Storage Area", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Outside Area", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Customer Area", LoadSceneMode.Additive);
+        if (!Application.isEditor)
+        {
+            // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+            //SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Workshop Area", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Storage Area", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Outside Area", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Customer Area", LoadSceneMode.Additive);
+        }
     }
 }
